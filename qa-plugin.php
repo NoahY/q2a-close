483e83dd9300cc69f505121a4a9b4016c8056003
NoahY <noahy57@gmail.com>
2011-09-17 10:00:28 +0530

    version

--- qa-plugin.php
+++ qa-plugin.php
@@ -4,7 +4,7 @@
         Plugin Name: Close Question
         Plugin URI: https://github.com/NoahY/q2a-Close
         Plugin Description: Close questions
-        Plugin Version: 0.1
+        Plugin Version: 1.0b
         Plugin Date: 2011-09-05
         Plugin Author: NoahY
         Plugin Author URI:                              
